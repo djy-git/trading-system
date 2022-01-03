67ee112597faa4d4e7104e582ec0da4b6d847655
ydj <djyoon1125@naver.com>
2022-01-03 21:17:08 +0900

    [REMOVE] 불필요한 __init__.py 제거

--- trading_system/CollectorEngine/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Import all modules in the ``CollectorEngine`` package
-"""
-
-from CollectorEngine.Engine import *
-from CollectorEngine.Engine_Y import *
-from CollectorEngine.Engine_J import *
-from CollectorEngine.Engine_L import *
--- trading_system/InvestorEngine/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Import all modules in the ``InvestorEngine`` package
-"""
-
-from InvestorEngine.Engine import *
-from InvestorEngine.Engine_Y import *
-from InvestorEngine.Engine_J import *
-from InvestorEngine.Engine_L import *
