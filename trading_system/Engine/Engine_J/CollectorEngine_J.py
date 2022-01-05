12d35f21094759bf1db53a920af936bccee3e160
jeongjaeyong <eoehd4108@gmail.com>
2022-01-05 20:59:22 +0900

    make NLP test

---
+++ trading_system/Engine/Engine_J/CollectorEngine_J.py
@@ -0,0 +1,13 @@
+from Engine.CollectorEngine import *
+from Engine.Engine_J.util import *
+
+
+class CollectorEngine_Y(CollectorEngine):
+    """정재용 엔진
+    """
+    def __init__(self, params):
+        super().__init__(params)
+
+
+    def collect_data(self):
+        pass
