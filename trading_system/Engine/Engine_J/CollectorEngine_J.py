6fcb2cd751ddc81bd0a1a8185f011e4705d9bcee
jeongjaeyong <eoehd4108@gmail.com>
2022-01-05 21:21:42 +0900

    [UPDATE] URL_READER

--- trading_system/Engine/Engine_J/CollectorEngine_J.py
+++ trading_system/Engine/Engine_J/CollectorEngine_J.py
@@ -2,12 +2,14 @@ from Engine.CollectorEngine import *
 from Engine.Engine_J.util import *
 
 
+for code_str in code_str_list:
+    url_list, subject_list = get_url_list(code_str)
 class CollectorEngine_Y(CollectorEngine):
     """정재용 엔진
     """
-    def __init__(self, params):
+    def __init__(self, params, code_str_list):
         super().__init__(params)
-
+        self.code_str_list = code_str_list
 
     def collect_data(self):
         pass
--- trading_system/Engine/Engine_J/util.py
+++ trading_system/Engine/Engine_J/util.py
@@ -5,12 +5,32 @@ import re
 from datetime import datetime
 import os
 
-code_str = "samsung%20electronic"
+code_str_list = ["samsung%20electronic"]
+
+'''
+원하는 종목과 관련된 기사 url 크롤링
+'''
 def get_url_list(code_str):
     news_url = 'https://www.koreatimes.co.kr/www2/common/search.asp?kwd=' + code_str
-    req = requests.get(news_url.format(query))
+    req = requests.get(news_url)
+
     soup = BeautifulSoup(req.text, 'html.parser')
-    ranks = soup.find_all('div', {'class': 'list_article_headline HD'})
-    print(ranks)
+    news_list = soup.find_all('div', {'class': 'list_article_headline HD'})
+    url_list = []
+    subject_list = []
+    for news in news_list:
+        url_list.append(news.find('a', href=True)['href'])
+        subject_list.append(str(news.text).replace('\n',''))
+    return url_list, subject_list
+
+
+'''
+각 기사 url로 부터 내용 크롤링 -> 일단 제목으로 먼지 처리 하는거 먼저 연습하자!
+'''
+def get_new(news_url):
+    req = requests.get(news_url)
+    soup = BeautifulSoup(req.text, 'html.parser')
+    news = soup.find_all('div', {'class': 'view_mid_div'})
     return
-get_url_list(code_str)
+
+
