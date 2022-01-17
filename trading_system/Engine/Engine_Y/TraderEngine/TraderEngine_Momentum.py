6f92a48e8e7c6671647d8117c862b13881bc7b83
djy-git <djyoon0223@gmail.com>
2022-01-17 23:33:47 +0900

    [UPDATE] Momentum 전략 class 추가

---
+++ trading_system/Engine/Engine_Y/TraderEngine/TraderEngine_Momentum.py
@@ -0,0 +1,19 @@
+from Engine.BaseTraderEngine import *
+from Engine.Engine_Y.util import *
+
+
+class TraderEngine_Momentum(BaseTraderEngine):
+    """윤동진 엔진
+
+    :param dict params: parameters
+    :param Client client: Client 정보
+    :ivar dict raw_datas: raw datas
+    """
+    def get_portfolio(self, trading_date):
+        """다음 시간의 포트폴리오를 선택
+
+        :param Timestamp trading_date: 거래 날짜
+        :return: 포트폴리오
+        :rtype: :class:`Trader.Portfolio`
+        """
+        return Portfolio(self.client.portfolio.get_holding_dic(), trading_date)
